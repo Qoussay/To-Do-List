231c6527c98fdcbcdafabb67832522827b63e332
Qoussay El Akkari <qoussay.akkari@gmail.com>
2021-10-17 16:39:52 +0100

    Made the list in an array where each element is displayed in its own line by passing the array we got from the user to the template and looping through it

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded());
 // Set ejs as the main view engine 
 app.set('view engine', 'ejs');
 
+var new_reminder_array = [];
+
 app.get('/' , (req,res) => {
     var today = new Date();
     
@@ -21,10 +23,16 @@ app.get('/' , (req,res) => {
         year: 'numeric',
     };
 
-    const day = today.toLocaleDateString('en-GB', options)
+    const day = today.toLocaleDateString('en-GB', options);
 
-    res.render('list', {day: day});
+    res.render('list', {day: day, new_list_item_array: new_reminder_array});
 });
+
+app.post('/', (req,res) => {
+    var new_reminder = req.body.new_reminder;
+    new_reminder_array.push(new_reminder);
+    res.redirect('/');
+})
 
 // Initialize the local server 
 app.listen(process.env.PORT || 8080, () =>
