dfc41d32a9981fb57bbdbfdc4103a33c03ffa5b3
Qoussay El Akkari <qoussay.akkari@gmail.com>
2021-10-17 16:15:03 +0100

    Change the date to be displayed as whole sentence. Removed the switch statement.

--- app.js
+++ app.js
@@ -13,34 +13,16 @@ app.set('view engine', 'ejs');
 
 app.get('/' , (req,res) => {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day;
+    
+    const options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+    };
 
-    switch (currentDay) {
-        case 0 :
-            day = 'Sunday';
-            break;
-        case 1 :
-            day = 'Monday';
-            break;
-        case 2 :
-            day = 'Tuesday';
-            break;
-        case 3 :
-            day = 'Wednesday';
-            break;
-        case 4 :
-            day = 'Thursday';
-            break;
-        case 5 :
-            day = 'Friday';
-            break;
-        case 6 :
-            day = 'Saturday';
-            break;
-        default:
-            console.log('An error occured with the switch statement.');
-    }
+    const day = today.toLocaleDateString('en-GB', options)
+
     res.render('list', {day: day});
 });
 
